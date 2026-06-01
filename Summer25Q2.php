947c677f026887fdbe28a8362d1ea5a4cf527ab8
Salman <rayhanartz@gmail.com>
2026-06-01 20:33:53 +0600

    Boiler Plate creation

--- Summer25Q2.php
+++ Summer25Q2.php
@@ -10,7 +10,28 @@
     <title>Movie Night</title>
   </head>
   <body>
-    
+    <form action="">
+
+
+      <h1>Movie Night Event</h1>
+      <label>Attendees</label>
+      <input type="number"></input>
+
+      <br><br>
+
+      <label>Seat Capacity</label>
+      <input type="number"></input>
+
+      <br><br>
+
+      <label>Ticket Price</label>
+      <input type="number"></input>
+
+      <br><br>
+      <input type="submit"></input>
+
+
+    </form>
   
   </body>
 </html>
