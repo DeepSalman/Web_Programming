e94584640ccb5b23f5ae0232384547677c0ca53c
Salman <rayhanartz@gmail.com>
2026-06-01 21:12:39 +0600

    Logic Building

--- Summer25Q2.php
+++ Summer25Q2.php
@@ -21,12 +21,12 @@
     <br><br>
 
     <label>Seat Capacity</label>
-    <input type="number" name="" id="seat_capacity"></input>
+    <input type="number" name="seat_capacity" id="seat_capacity" required>
 
     <br><br>
 
     <label>Ticket Price</label>
-    <input type="number" name="" id="ticket_price"></input>
+    <input type="number" name="ticket_price" id="ticket_price" required>
 
     <br><br>
     <input type="submit"></input>
@@ -43,13 +43,18 @@
 </html>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $attendees = $_POST['attendees'];
   $seat_capacity = $_POST['seat_capacity'];
   $ticket_price = $_POST['ticket_price'];
 
-  
+  $total_screen = (int) ceil($attendees / $seat_capacity);
+
+  $empty_seats =($total_screen * $seat_capacity)-$attendees;
+
+  $wasted_money=($ticket_price*$empty_seats);
 
   
+
 }
 ?>
