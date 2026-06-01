9e3cd41ebc96b25d8bdbdad5ae0a1bd19057801f
Salman <rayhanartz@gmail.com>
2026-06-01 22:16:52 +0600

    DB connect

---
+++ Summer25Q3.php
@@ -0,0 +1,22 @@
+<?php 
+$DB_HOST= "localhost";
+$DB_USER = "root";
+$DB_PASS ="";
+$DB_NAME = "sundarban";
+
+$connection = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
+
+
+if(!$connection){
+    die("connection Failed". mysqli_connect_error());
+}
+
+
+
+
+
+mysqli_close($connection)
+
+
+
+?>
