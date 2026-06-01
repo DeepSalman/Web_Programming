ec041c13980fca6c2c54e455c13e3a4f16e50a4c
Salman <rayhanartz@gmail.com>
2026-06-01 20:54:13 +0600

    getting the values from userend

--- Summer25Q2.php
+++ Summer25Q2.php
@@ -1,37 +1,55 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href=""
-      rel="stylesheet"
-    />
-    <title>Movie Night</title>
-  </head>
-  <body>
-    <form action="">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    href=""
+    rel="stylesheet" />
+  <title>Movie Night</title>
+</head>
+
+<body>
+  <form action="" method="POST">
 
 
-      <h1>Movie Night Event</h1>
-      <label>Attendees</label>
-      <input type="number"></input>
+    <h1>Movie Night Event</h1>
+    <label>Attendees</label>
+    <input type="number" name="attendees" id=""></input>
 
-      <br><br>
+    <br><br>
 
-      <label>Seat Capacity</label>
-      <input type="number"></input>
+    <label>Seat Capacity</label>
+    <input type="number" name="" id="seat_capacity"></input>
 
-      <br><br>
+    <br><br>
 
-      <label>Ticket Price</label>
-      <input type="number"></input>
+    <label>Ticket Price</label>
+    <input type="number" name="" id="ticket_price"></input>
 
-      <br><br>
-      <input type="submit"></input>
+    <br><br>
+    <input type="submit"></input>
 
 
-    </form>
-  
-  </body>
+
+
+  </form>
+  <h1>
+  </h1>
+
+</body>
+
 </html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $attendees = $_POST['attendees'];
+  $seat_capacity = $_POST['seat_capacity'];
+  $ticket_price = $_POST['ticket_price'];
+
+  
+
+  
+}
+?>
