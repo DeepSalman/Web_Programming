60567d7cbee8ad618ebd60b7685a5f0fd8ed62b3
Salman <rayhanartz@gmail.com>
2026-06-02 11:55:49 +0600

    Question 1 solve push

--- Summer25Q3.php
+++ Summer25Q3.php
@@ -11,11 +11,36 @@ if(!$connection){
     die("connection Failed". mysqli_connect_error());
 }
 
+$query = "SELECT * FROM sales_data";
+$result = mysqli_query($connection,$query); ///Selected table will be fetched 
+
+
+//Question 1 solution
+$categoryRevenue=[];
+
+while($row=mysqli_fetch_assoc($result)){
+    $category = $row['CategoryName'];
+    $revenue = $row['Revenue'];
+
+    if(!isset($categoryRevenue[$category])){
+        $categoryRevenue[$category]=0;
+    }
+
+    $categoryRevenue[$category] += $revenue;
+
+
+}
+
+echo "<h1>1.Revenue Per category: </h1>";
+foreach($categoryRevenue as $category => $total){
+    echo $category . ":".$total."<br>";
+}
 
 
 
 
-mysqli_close($connection)
+mysqli_close($connection);
+
 
 
 
