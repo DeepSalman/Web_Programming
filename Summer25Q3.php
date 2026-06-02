297dbb4db2440eca4a0a6423ecf4e049277743a4
Salman <rayhanartz@gmail.com>
2026-06-02 13:09:29 +0600

    Question 04

--- Summer25Q3.php
+++ Summer25Q3.php
@@ -18,6 +18,7 @@ $result = mysqli_query($connection, $query); ///Selected table will be fetched
 //Question 1 solution
 $categoryRevenue = [];
 
+
 while ($row = mysqli_fetch_assoc($result)) {
     $category = $row['CategoryName'];
     $revenue = $row['Revenue'];
@@ -70,8 +71,40 @@ mysqli_query(
 //Question 3 ends here 
 
 
+//Question 4 starts here
+echo "<h1>4.</h1>";
+$result2 = mysqli_query($connection, $query);
+
+//Counting category
+$categoryCount=[];
+while ($row = mysqli_fetch_assoc($result2)) {
+    $category = $row['CategoryName'];
+    $revenue = $row['Revenue'];
+
+    if (!isset($categoryCount[$category])) {
+        $categoryCount[$category]=0;
+    }
+
+    $categoryCount[$category] ++;
+}
+
+$result2 = mysqli_query($connection, $query);
+while ($row = mysqli_fetch_assoc($result2)){
+    $name = $row['ProductName'];
+    $category = $row['CategoryName'];
 
 
+    echo "Name: ".$name.", CategoryName: ".$category.", Label: ";
+    $averageRevenue=$categoryRevenue[$category]/$categoryCount[$category];
+    
+    if($row['Revenue']>$averageRevenue){
+        echo "TOP SELLER <br>";
+    }
+    else{
+        echo "Normal seller <br>";
+    }
+
+}
 
 
 
