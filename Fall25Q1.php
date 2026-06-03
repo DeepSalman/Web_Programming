764576a055b4f5e96ab38671d45552744122015d
Salman <rayhanartz@gmail.com>
2026-06-03 18:47:20 +0600

    Upper structure push

---
+++ Fall25Q1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href=""
+      rel="stylesheet"
+    />
+    <title>
+        Pass Marks
+    </title>
+  </head>
+  <body>
+    <form action="" method="">
+        <label>CT1 :</label>
+        <input type="number" name="ct1" required >
+        <br><br>
+
+        <label>CT2 :</label>
+        <input type="number" name="ct1" required>
+        <br><br>
+
+        <label>CT3 :</label>
+        <input type="number" name="ct1" required>
+        <br><br>
+
+        <label>MID :</label>
+        <input type="number" name="ct1" required>
+        <br><br>
+
+        <label>Final :</label>
+        <input type="number" name="ct1" required>
+        <br><br>
+
+
+    </form>
+
+  </body>
+</html>
