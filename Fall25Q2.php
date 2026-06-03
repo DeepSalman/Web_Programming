b16ac566637f7123ec985937524d414202876bac
Salman <rayhanartz@gmail.com>
2026-06-03 19:38:05 +0600

    Update terms

---
+++ Fall25Q2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+        href=""
+        rel="stylesheet" />
+    <title>Event Planning</title>
+</head>
+
+<body>
+
+    <form action="" method="POST">
+        <h2>Event Planning System</h2>
+        <label>Items sold :</label>
+        <input type="number" name="ct1" id="ct1" required>
+        <br><br>
+
+        <label>Number of  :</label>
+        <input type="number" name="ct2" required>
+        <br><br>
+
+        <label>Target :</label>
+        <input type="number" name="ct3" required>
+        <br><br>
+
+        
+
+        <button type="submit">Calculate Total</button>
+
+    </form>
+</body>
+
+</html>
